8f44c11f929873c6b5f36ed9253d178fbe02f16a
Italo Araujo <italojsouza@gmail.com>
2024-04-15 15:42:37 -0300

    feat: Create Endpoint POST that associates crop to fertilizer

--- src/main/java/com/betrybe/agrix/controller/CropController.java
+++ src/main/java/com/betrybe/agrix/controller/CropController.java
@@ -1,14 +1,17 @@
 package com.betrybe.agrix.controller;
 
 import com.betrybe.agrix.controller.dto.CropDto;
+import com.betrybe.agrix.controller.dto.FertilizerDto;
 import com.betrybe.agrix.entity.Crop;
 import com.betrybe.agrix.service.CropService;
 import com.betrybe.agrix.service.exception.CropNotFoundException;
+import com.betrybe.agrix.service.exception.FertilizerNotFoundException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -60,6 +63,32 @@ public class CropController {
     List<Crop> crops = cropService.findCropsBetweenDates(start, end);
     return crops.stream()
         .map(CropDto::fromEntity)
+        .toList();
+  }
+
+  /**
+   * Associates a crop with a fertilizer.
+   */
+  @PostMapping("/{cropId}/fertilizers/{fertilizerId}")
+  @ResponseStatus(HttpStatus.CREATED)
+  public String associateCropWithFertilizer(
+      @PathVariable Long cropId,
+      @PathVariable Long fertilizerId
+  ) throws CropNotFoundException, FertilizerNotFoundException {
+    cropService.associateCropWithFertilizer(cropId, fertilizerId);
+    return "Fertilizante e plantação associados com sucesso!";
+  }
+
+  /**
+   * Returns a list of all fertilizers for a crop.
+   */
+  @GetMapping("/{cropId}/fertilizers")
+  @ResponseStatus(HttpStatus.OK)
+  public List<FertilizerDto> getFertilizersByCropId(@PathVariable Long cropId)
+      throws CropNotFoundException {
+    Crop crop = cropService.findById(cropId);
+    return crop.getFertilizers().stream()
+        .map(FertilizerDto::fromEntity)
         .toList();
   }
 }
--- src/main/java/com/betrybe/agrix/service/CropService.java
+++ src/main/java/com/betrybe/agrix/service/CropService.java
@@ -1,8 +1,11 @@
 package com.betrybe.agrix.service;
 
 import com.betrybe.agrix.entity.Crop;
+import com.betrybe.agrix.entity.Fertilizer;
 import com.betrybe.agrix.repository.CropRepository;
+import com.betrybe.agrix.repository.FertilizerRepository;
 import com.betrybe.agrix.service.exception.CropNotFoundException;
+import com.betrybe.agrix.service.exception.FertilizerNotFoundException;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +18,12 @@ import org.springframework.stereotype.Service;
 public class CropService {
 
   private final CropRepository cropRepository;
+  private final FertilizerRepository fertilizerRepository;
 
   @Autowired
-  public CropService(CropRepository cropRepository) {
+  public CropService(CropRepository cropRepository, FertilizerRepository fertilizerRepository) {
     this.cropRepository = cropRepository;
+    this.fertilizerRepository = fertilizerRepository;
   }
 
   public List<Crop> findAll() {
@@ -30,15 +35,31 @@ public class CropService {
   }
 
   /**
-   * Find crops between two dates.
-   *
-   * @param start the start date
-   * @param end the end date
-   * @return the list of crops between the two dates
+   * Returns a list of crops between two dates.
    */
   public List<Crop> findCropsBetweenDates(String start, String end) {
     LocalDate startDate = LocalDate.parse(start);
     LocalDate endDate = LocalDate.parse(end);
     return cropRepository.findCropsBetweenDates(startDate, endDate);
+  }
+
+  /**
+   * Associates a crop with a fertilizer.
+   */
+  public void associateCropWithFertilizer(Long cropId, Long fertilizerId)
+      throws CropNotFoundException, FertilizerNotFoundException {
+    Crop crop = cropRepository.findById(cropId).orElseThrow(CropNotFoundException::new);
+    Fertilizer fertilizer = fertilizerRepository.findById(fertilizerId)
+        .orElseThrow(FertilizerNotFoundException::new);
+    crop.getFertilizers().add(fertilizer);
+    cropRepository.save(crop);
+  }
+
+  /**
+   * Returns a list of all fertilizers associated with a crop.
+   */
+  public List<Fertilizer> findFertilizersByCropId(Long cropId) throws CropNotFoundException {
+    Crop crop = cropRepository.findById(cropId).orElseThrow(CropNotFoundException::new);
+    return crop.getFertilizers();
   }
 }
