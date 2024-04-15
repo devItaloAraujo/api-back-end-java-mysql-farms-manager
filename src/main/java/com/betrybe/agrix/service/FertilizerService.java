c5d02a29edaff80e793d2d21f9ef6d643d805568
Italo Araujo <italojsouza@gmail.com>
2024-04-15 15:29:34 -0300

    feat: Add FertilizerRepository and Service

---
+++ src/main/java/com/betrybe/agrix/service/FertilizerService.java
@@ -0,0 +1,35 @@
+package com.betrybe.agrix.service;
+
+import com.betrybe.agrix.entity.Fertilizer;
+import com.betrybe.agrix.repository.FertilizerRepository;
+import com.betrybe.agrix.service.exception.FertilizerNotFoundException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * Service for the Fertilizer entity.
+ */
+@Service
+public class FertilizerService {
+
+  private final FertilizerRepository fertilizerRepository;
+
+  @Autowired
+  public FertilizerService(FertilizerRepository fertilizerRepository) {
+    this.fertilizerRepository = fertilizerRepository;
+  }
+
+  public Fertilizer create(Fertilizer fertilizer) {
+    return fertilizerRepository.save(fertilizer);
+  }
+
+  public List<Fertilizer> findAll() {
+    return fertilizerRepository.findAll();
+  }
+
+  public Fertilizer findById(Long id) throws FertilizerNotFoundException {
+    return fertilizerRepository.findById(id).orElseThrow(FertilizerNotFoundException::new);
+  }
+}
